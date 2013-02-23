5bd4a65950ce26b4c36f6ba406eb3ead7c5b623b
Mykel Nahorniak <myke@localist.com>
2013-02-23 14:21:24 -0500

    Revert "- fixed bootstrap"
    This reverts commit 9ac47637743e638d16eb68c5721ca56ac26e2347.
    
    Conflicts:
    
    	Tempo.sublime-workspace

--- _header.php
+++ _header.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width">
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
 
