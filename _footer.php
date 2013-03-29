feb8e9f9d5ab911a132cb903e5aea30aa3ddcd52
Mykel Nahorniak <myke@localist.com>
2013-03-29 16:56:25 -0400

    - added GA tracker

--- _footer.php
+++ _footer.php
@@ -2,15 +2,25 @@
 
       <hr>
 
-<!-- Main hero unit for a primary marketing message or call to action -->
-<div class="alert alert-info">
-  <b>Tempo</b> is made for cyclists who use power meters. It's an automated coach designed to use your power data as a guide to minimize weaknesses while keeping strengths at their peak. Workouts are created on the fly and take seasonality, your power profile, and other factors into account.
-</div>
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="alert alert-info">
+        <b>Tempo</b> is made for cyclists who use power meters. It's an automated coach designed to use your power data as a guide to minimize weaknesses while keeping strengths at their peak. Workouts are created on the fly and take seasonality, your power profile, and other factors into account.
+      </div>
 
       <footer>
         <p>&copy; Mykel Nahorniak</p>
       </footer>
 
     </div> <!-- /container -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-39725016-1', 'herokuapp.com');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
